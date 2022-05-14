7cce83d72835d35b5976b85bb256faa57ff470c1
henrygilbert22 <hg19b@my.fsu.edu>
2022-05-14 12:29:51 -0500

    Kadka working

---
+++ proj1/pub.py
@@ -0,0 +1,25 @@
+import kafka_helper
+
+from kafka import KafkaProducer
+
+class Pub:
+
+    k_utility: kafka_helper.Kafka
+    producer: KafkaProducer
+
+    topic: str
+
+    def __init__(self):
+        
+        self.k_utility = kafka_helper.Kafka('10.0.0.1', '2181')
+        self.producer = self.k_utility.get_producer()
+    
+    def publish(self, topic: str, data: str):
+        
+        if not self.k_utility.topic_exists(topic):
+            self.k_utility.create_topic(topic, 1, 1)
+
+        print(f"publishing {data} to {topic}")
+        self.producer.send(topic, str.encode(str(data)))
+
+    
---
+++ proj1/sub.py
@@ -0,0 +1,37 @@
+import kafka_helper
+
+from kafka import KafkaConsumer
+from time import sleep
+
+class Sub:
+
+    consumer: KafkaConsumer
+    k_utility: kafka_helper.Kafka
+
+    topic: str
+
+    def __init__(self, topic: str):
+        
+        self.topic = topic
+
+        self.k_utility = kafka_helper.Kafka('10.0.0.1', '2181')
+        self.wait_for_topic_creation()
+
+        self.consumer = self.k_utility.get_consumer(self.topic)
+
+    def wait_for_topic_creation(self):
+
+        while True:
+
+            if self.k_utility.topic_exists(self.topic):
+                break
+
+            else:
+                sleep(1)
+                print("waiting for topic to be created")
+
+
+    def recieve(self):
+
+        for message in self.consumer:
+            print(message)
